7fa0820cf2e90023a2ae91ecc2fb390fcaee4158
Tuan Bang <21012527@st.phenikaa-uni.edu.vn>
2024-05-27 08:11:49 +0700

    commit

---
+++ Student-Management-main/StudentManagementPlus/ConnectionString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementPlus {
+    public class ConnectionString {
+        public static string connectionString = @"Data Source=TUAN\TUAN;Initial Catalog=quanlysinhvien;Integrated Security=True;";
+    }
+}
